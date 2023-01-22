108e90b4792d3630fbc2d3b28401e9d56758c647
Niladri Das <boney0310@gmail.com>
2023-01-23 02:21:29 +0530

    modified stack

--- header_files/stack.h
+++ header_files/stack.h
@@ -1,30 +1,50 @@
+/**
+ * Standard Stack implementation via linked list.
+ * Stores a floating point number as data.
+ * See ../testing/StackDriver for usecases.
+*/
+
 #include<stdio.h>
 #include<stdlib.h>
 
+/**
+ * Stack node implemented via linked list.
+*/
 typedef struct Stack{
-    char arr[5];
+    float ch;
     struct Stack* next;
 }Stack;
 
-Stack* createNode(char *arr)
+Stack* createNode(float ch)
 {
     Stack* temp = (Stack*)malloc(sizeof(Stack));
     int i=-1;
-    while(arr[++i]!='$' && i<5)
-    temp->arr[i] = arr[i];
-    temp->arr[i]='$';
+    temp->ch = ch;
     temp->next = NULL;
     return temp;
 }
 
-Stack* push(Stack* top, char *arr)
+/**
+ * Pushes a node onto the stack.
+ * @param top A Stack* pointer pointing to the top of the stack
+ * @param ch number to be stored as data
+ * @return new Stack* top pointer
+*/
+Stack* push(Stack* top, float ch)
 {
-    Stack* temp = createNode(arr);
+    Stack* temp = createNode(ch);
     temp->next = top;
     top = temp;
     return top;
 }
 
+/**
+ * Pops a node from the top of the stack.
+ * 
+ * Note: Data from the top of the stack, if required, should be accessed via top->ch before popping.
+ * @param top A Stack* pointer pointing to the top of the stack
+ * @return new Stack* top pointer after popping a node
+*/
 Stack* pop(Stack* top)
 {
     Stack* temp = top;
@@ -33,17 +53,26 @@ Stack* pop(Stack* top)
     return top;
 }
 
-
+/**
+ * Checks if stack is empty or not.
+ * @param top A Stack* pointer pointing to the top of the stack
+ * @return 1 if empty, 0 otherwise.
+*/
 int isEmpty(Stack *top)
 {
     return top==NULL?1:0;
 }
 
+/**
+ * Prints the stack.
+ * @param top A Stack* pointer pointing to the top of the stack
+ * 
+*/
 void printStack(Stack *top)
 {
     while(isEmpty(top)!=1)
     {
-        printf("%s\n",top->arr);
+        printf("%f\n",top->ch);
         top = top->next;
     }
 }
--- testing/StackDriver.c
+++ testing/StackDriver.c
@@ -3,16 +3,13 @@
 
 int main()
 {
-    char arr[] = {'a','b','\0'};
-    char arr2[] = {'a','b','c','\0'};
-    char arr3[] = {'2','1','\0'};
-    char arr4[] = {'1','b','#','\0'};
+    float num1=2, num2=3, num3=4, num4=5;
     Stack *top = NULL;
     int i=-1;
-    top = push(top,arr);
-    top = push(top,arr2);
-    top = push(top,arr3);
-    top = push(top,arr4);
+    top = push(top,num1);
+    top = push(top,num2);
+    top = push(top,num3);
+    top = push(top,num4);
     printStack(top);
     top = pop(top);
     top = pop(top);
