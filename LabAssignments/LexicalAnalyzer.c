a3b7dd266d7168ff23a98e910061a3781f97c45e
Niladri Das <86772172+SegFault03@users.noreply.github.com>
2023-02-01 12:53:24 +0530

    Update LexicalAnalyzer.c

--- LabAssignments/LexicalAnalyzer.c
+++ LabAssignments/LexicalAnalyzer.c
@@ -7,11 +7,23 @@
 void getKeyword(char *word)
 {
     int i;
-    char *numbers[] = {"auto","break",	"case",	"char",	"const",	"continue",	"default",	"do",
+    char *keywords[] = {"auto","break",	"case",	"char",	"const",	"continue",	"default",	"do",
 "double",	"else",	"enum",	"extern",	"float",	"for",	"goto",	"if",
 "int",	"long",	"register",	"return",	"short",	"signed",	"sizeof",	"static",
 "struct",	"switch",	"typedef",	"union",	"unsigned",	"void",	"volatile",	"while", ""}, **n;
-  n = numbers;
+  n = keywords;
+  while (*n != "") {
+    if(strcmp(*n,word)==0)
+    printf("\n%s",word);
+    n++;
+  }
+}
+
+void getOperators(char *word)
+{
+    char *delimeters[] =  {"+","/","-","*","%","++","--","=","+=","-=","/=","*=","%=","&=",
+    "|=","^=",">>=","<<="}, **n;
+  n = delimeters;
   while (*n != "") {
     if(strcmp(*n,word)==0)
     printf("\n%s",word);
@@ -29,7 +41,13 @@ int main()
     char tempwrd[10];
     for(i=0;i<strlen(out);i++)
     {
-        if(out[i]==' '||out[i]==';'||out[i]=='\n')
+        if((out[i]>=48 && out[i]<=57 )|| (out[i]>=97 && out[i]<=122))
+        {
+            tempwrd[c] = out[i];
+            c++;
+        }
+        
+        else if(out[i]==' '||out[i]==';'||out[i]=='\n')
         {
             tempwrd[c] = '\0';
             getKeyword(tempwrd);
@@ -37,8 +55,9 @@ int main()
         }
         else
         {
-            tempwrd[c] = out[i];
-            c++;
+            char str[2] = "\0"; /* gives {\0, \0} */
+            str[0] = out[i];
+            getOperators(str);
         }
     }
 }
